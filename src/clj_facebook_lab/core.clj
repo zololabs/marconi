8274e4f341ab1d56608c3c550cce4ab2fdc64362
Siva Jagadeesan <sivajag@gmail.com>
2012-04-20 16:25:34 -0700

    initial checkin

---
+++ src/clj_facebook_lab/core.clj
@@ -0,0 +1,1 @@
+(ns clj-facebook-lab.core)
---
+++ test/clj_facebook_lab/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-facebook-lab.test.core
+  (:use [clj-facebook-lab.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
