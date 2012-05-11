45e5a755ba69fe3c90219ac27831f8701acc655b
Siva Jagadeesan <sivajag@gmail.com>
2012-05-11 15:04:13 -0700

    added simple dummy data factories

--- src/zolodeck/clj_social_lab/facebook/data_factory.clj
+++ src/zolodeck/clj_social_lab/facebook/data_factory.clj
@@ -1,8 +1,32 @@
 (ns zolodeck.clj-social-lab.facebook.data-factory)
 
+(defn- gender []
+  (if (zero? (rand-int 2)) "male" "female"))
+
+(defn- last-name [id]
+  (str "last_name_" id))
+
+(defn- first-name [id]
+  (str "first_name_" id))
+
+(defn user []
+ (let [id (rand-int 100000000)]
+   {:gender (gender),
+    :last_name (last-name id)
+    :link (str "http://www.facebook.com/" id),
+    :timezone -7,
+    :name (str (first-name id) " " (last-name id)),
+    :locale "en_US",
+    :username (str "username_" id),
+    :email (str id "@gmail.com"),
+    :updated_time "2012-02-17T17:36:14+0000",
+    :first_name (first-name id)
+    :verified true,
+    :id (str id)}))
+
 (defn friend []
   (let [id (rand-int 100000000)]
-    {:gender (if (zero? (rand-int 2)) "male" "female")
+    {:gender (gender)
      :last_name (str "last_name_" id)
      :link (str "http://www.facebook.com/profile.php?id=" id),
      :installed true,
