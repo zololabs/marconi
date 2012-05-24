9fe65f849bf1fbcfb0f6552a66c74866fca6dc20
Amit Rathore <amitrathore@gmail.com>
2012-05-23 20:14:46 -0700

    minor refactoring of api.clj

--- src/zolodeck/clj_social_lab/facebook/api.clj
+++ src/zolodeck/clj_social_lab/facebook/api.clj
@@ -27,8 +27,10 @@
 (def DEFAULT-PERMISSIONS "email,friends_about_me,friends_birthday,friends_relationship_details,friends_location,friends_likes,friends_website,read_mailbox,offline_access")
 
 (defn run-fql [auth-token query-string]
-  (:body (fb-auth/with-facebook-auth {:access-token auth-token} 
-           (fb-client/get :fql {:fql query-string}))))
+  (->> {:fql query-string}
+       (fb-client/get :fql)
+       (fb-auth/with-facebook-auth {:access-token auth-token})
+       :body))
 
 (defn all [app-id app-access-token]
   (->> (fb-request/empty-user-request app-access-token)
@@ -39,8 +41,7 @@
        (map keywordize-map)))
 
 (defn user-from-fb [auth-token]
-  (-> (fb-auth/with-facebook-auth {:access-token auth-token} 
-        (fb-client/get [:me]))
+  (-> (fb-auth/with-facebook-auth {:access-token auth-token} (fb-client/get [:me]))
       :body
       (assoc :access-token auth-token)))
 
@@ -55,19 +56,15 @@
                     :extract :data 
                     :paging true})))
 
-(defn inbox-fql []
-  "SELECT thread_id FROM thread WHERE folder_id = 0 ")
-
-(defnk message-fql [:start-time BEGINNING-OF-TIME :thread-id nil]
-  (if-not thread-id (throw+ {:severity :high :reason :missing-value :field :thread-id}))
+(defn message-fql [thread-id]
   (str "SELECT message_id, thread_id, author_id, body, created_time, attachment, viewer_id FROM message WHERE thread_id = " thread-id))
 
 (defn fetch-thread-messages [auth-token thread-id]
-  (run-fql auth-token (message-fql :thread-id thread-id)))
+  (run-fql auth-token (message-fql thread-id)))
 
 (defn fetch-messages [{:keys [access-token]}]
   (print-vals "Access-token:" access-token)
-  (->> (inbox-fql)
+  (->> "SELECT thread_id FROM thread WHERE folder_id = 0"
        (run-fql access-token)
        (map :thread_id)
        (mapcat (partial fetch-thread-messages access-token))))
