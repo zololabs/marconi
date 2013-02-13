09627af36a9d262bc9a2371b1702a3519c8811b0
Amit Rathore <amitrathore@gmail.com>
2013-02-12 23:19:09 -0800

    tests no longer hit FB, and any boundary API testing should be done as domain-specific in the apps using clj-social-lab for testing

--- test/zolodeck/clj_social_lab/facebook/core_test.clj
+++ test/zolodeck/clj_social_lab/facebook/core_test.clj
@@ -2,48 +2,26 @@
   (:use [clojure.test :only [run-tests deftest is are testing]]
         zolodeck.clj-social-lab.facebook.core
         zolodeck.utils.system
-        zolodeck.utils.debug)
-  (:require [zolodeck.clj-social-lab.facebook.api :as api]))
-
-(defmacro in-test-env [& body]
-  `(binding [api/APP-ID (system-env "SOCIAL_LAB_TEST_APP_ID")
-             api/APP-SECRET (system-env "SOCIAL_LAB_TEST_APP_SECRET")
-             api/APP-ACCESS-TOKEN (api/app-access-token
-                                   (system-env "SOCIAL_LAB_TEST_APP_ID")
-                                   (system-env "SOCIAL_LAB_TEST_APP_SECRET"))]
-     (do ~@body)))
+        zolodeck.utils.debug))
 
 (deftest test-basic-fb-functionality
-  (in-test-env
-   (in-facebook-lab 
-    (let [jack (create-user "Jack" "Mulder")
-          jill (create-user "Jill" "Sculley")
-          joe (create-user "Joe" "Conrad")]
+  (in-facebook-lab 
+   (let [jack (create-user "Jack" "Mulder")
+         jill (create-user "Jill" "Sculley")
+         joe (create-user "Joe" "Conrad")]
 
-      (make-friend jack jill)
-      (make-friend jack joe)
-      (is (= 2 (count (fetch-friends jack))))
+     (make-friend jack jill)
+     (make-friend jack joe)
+     (is (= 2 (count (fetch-friends jack))))
 
-      (let [dummy-friend (first (sort-by :first_name (fetch-friends jack)))]
-        (are [expected attrib] (= expected (dummy-friend attrib))
-             "Jill" :first_name
-             "Sculley" :last_name))
+     (let [dummy-friend (first (sort-by :first_name (fetch-friends jack)))]
+       (are [expected attrib] (= expected (dummy-friend attrib))
+            "Jill" :first_name
+            "Sculley" :last_name))
 
-      (send-message jack jill "1" "Hey Jill, I am going to fetch" "2012-05-01")
-      (send-message jill jack "1" "Sure I will come" "2012-05-02")
-      (send-message jack jill "1" "Great see you soon" "2012-05-03")
-      (is (= 3 (count (fetch-messages jack))))
-
-      (let [an-fb-user (first (api/all-users))
-
-            an-fb-friend (first (api/friends-list an-fb-user))
-            a-dummy-friend (first (fetch-friends jack))
-
-            an-fb-message (first (api/fetch-messages an-fb-user))
-            a-dummy-message (first (fetch-messages jack))]
-        
-        (is (= (sort (keys an-fb-user)) (sort (keys jack))))
-        (is (= (sort (keys an-fb-friend)) (sort (keys a-dummy-friend))))
-        (is (= (sort (keys an-fb-message)) (sort (keys a-dummy-message)))))))))
+     (send-message jack jill "1" "Hey Jill, I am going to fetch" "2012-05-01")
+     (send-message jill jack "1" "Sure I will come" "2012-05-02")
+     (send-message jack jill "1" "Great see you soon" "2012-05-03")
+     (is (= 3 (count (fetch-messages jack)))))))
 
 
